773a303873fda464a840cef48adceb6a0b7305d5
pedrorcruzz <phrosa.dev@gmail.com>
2025-05-24 00:23:14 -0300

    feat: create entity for user

--- src/entities/user.go
+++ src/entities/user.go
@@ -1,1 +1,15 @@
 package entities
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"ID"`
+	Email     string             `bson:"email" json:"email"`
+	Passoword string             `bson:"password" json:"-"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+}
