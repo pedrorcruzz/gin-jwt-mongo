eb2adac1a996e53eafcef01ca12978d5780005b3
pedrorcruzz <phrosa.dev@gmail.com>
2025-05-24 00:23:35 -0300

    feat: create entity for task

---
+++ src/entities/task.go
@@ -0,0 +1,15 @@
+package entities
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Task struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"ID"`
+	Title       string             `bson:"title" json:"title"`
+	Description string             `bson:"description" json:"description"`
+	CreatedAt   time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt   time.Time          `bson:"updated_at" json:"updated_at"`
+}
